5e3a92ec815d6b425f95a9a553a37b9e34215b0a
Cory Martin <murribu@gmail.com>
2018-10-29 22:50:49 -0500

    grab all sessions!

---
+++ lambda/reinventGrabCatalog/README.md
@@ -0,0 +1,12 @@
+Session Type Ids
+
+Workshop = 1000
+Spotlight Lab = 1780
+Session = 2
+Lightning Talk = 1440
+Hackathon = 1040
+Demo Session = 1560
+Chalk Talk = 1700
+Builders Session = 1781
+After Hours = 1921
+Activity = 1920
--- lambda/reinventGrabCatalog/lambda_function.py
+++ lambda/reinventGrabCatalog/lambda_function.py
@@ -2,6 +2,7 @@ import json
 import requests
 import boto3
 import os
+from datetime import datetime
 from bs4 import BeautifulSoup
 from boto3.dynamodb.conditions import Key, Attr
 
@@ -9,6 +10,7 @@ def lambda_handler(event, context):
     dynamo = boto3.resource('dynamodb')
     table = dynamo.Table(os.environ["dynamotable"])
 
+    sessionTypeIDs = ['1000', '1780', '2', '1440', '1040', '1560', '1700', '1781', '1921', '1920']
     retval = []
 
     headers = {
@@ -26,16 +28,55 @@ def lambda_handler(event, context):
         "referer": "https://www.portal.reinvent.awsevents.com/connect/search.ww"
     }
 
-    retval.append(headers["cookie"])
+    # retval.append(headers["cookie"])
 
-    data = {"searchPhrase": "", "searchType": "session", "tc": "0", "sortBy": "abbreviationSort", "p": ""}
-    page = requests.post("https://www.portal.reinvent.awsevents.com/connect/processSearchFilters.do", headers=headers, data=data)
-    
-    content = BeautifulSoup(page.text, 'html.parser')
-    
-    s3 = boto3.client('s3')
-    s3.put_object(Body=page.text, Bucket='reinvent-grabs', Key='1.html')
+    i = 1
+    for sessionTypeID in sessionTypeIDs:
+        data = {"searchPhrase": "", "searchType": "session", "tc": "0", "sortBy": "abbreviationSort", "p": "", "sessionTypeID": sessionTypeID}
+        page = requests.post("https://www.portal.reinvent.awsevents.com/connect/processSearchFilters.do", headers=headers, data=data)
         
+        content = BeautifulSoup(page.text, 'html.parser')
+        
+        s3 = boto3.client('s3')
+        s3.put_object(Body=page.text, Bucket='reinvent-grabs', Key='1.html')
+            
+        processContentAndSave(table, content)
+
+        #second call
+
+        keepLooping = True
+        while keepLooping:
+            i += 1
+            awsalb = ''
+            # sessionidconnect = ''
+            retval.append("\ni = %d" % i)
+            retval.append(page.headers['Set-Cookie'])
+            for entry in page.headers['Set-Cookie'].split('; '):
+                # retval.append(entry.split('=')[0])
+                if (entry.split('=')[0] == 'AWSALB'):
+                    awsalb = entry.split('=')[1]
+                if (entry.split('=')[0] == 'Path' and entry.split('=')[1] == '/, SESSIONIDconnect'):
+                    sessionidconnect = entry.split('=')[2]
+
+            headers["cookie"] = "DWRSESSIONID=nNNQ8ZylsbVlrLX1TUpsMzSNppm; _ga=GA1.2.603512740.1498063034; s_fid=4DF3153FC00FF9A8-00D998B2CA4F09CE; regStatus=pre-register; session-set=true; aws-priv=eyJ2IjoxLCJzdCI6MX0=; __atssc=google%3B1; s_campaign=em%7Cawsreinvent_reg_reminder%7Cem_84172%7Cevent_ev_reinvent%7Cevent%7Cglobal%7Cmult%7Cem_84172; c_m=emundefinedEmailundefined; s_cc=true; s_eVar60=em_84172; cookieAgreement=agreed; SESSIONIDportal=aaaZC9WE1bIhL_m8YXzzw; s_vn=1561578928706%26vn%3D5; s_sq=%5B%5BB%5D%5D; s_dslv=1539197425868; s_nr=1539197425870-Repeat; SESSIONIDconnect=" + sessionidconnect + "; __atuvc=0%7C37%2C0%7C38%2C0%7C39%2C0%7C40%2C34%7C41; __atuvs=5bc2a4f5bfc9bf9e000; AWSALB=" + awsalb
+
+            # retval.append(headers["cookie"])
+            
+            data = {"searchType": "session", "more": "true"}
+            page = requests.post("https://www.portal.reinvent.awsevents.com/connect/processSearchFilters.do", headers=headers, data=data)
+
+            content = BeautifulSoup(page.text, 'html.parser')
+            
+            s3 = boto3.client('s3')
+            s3.put_object(Body=page.text, Bucket='reinvent-grabs', Key='%d.html' % i)
+
+            row_count = processContentAndSave(table, content)
+            retval.append('row_count = %d' % row_count)
+            keepLooping = row_count > 0
+
+    return retval
+
+def processContentAndSave(table, content):
     resultRows = content.select('.resultRow')
 
     for resultRow in resultRows:
@@ -53,45 +94,29 @@ def lambda_handler(event, context):
             'track': track,
             # "html": str(resultRow)
         }
+        print(row_data['id'])
         # if len(str(resultRow.find_all("small", {"class": "speakers"})[0])) > 32:
         #     return str(''.join(map(str,resultRow.find_all("small", {"class": "speakers"})[0].contents)))
         response = table.query(KeyConditionExpression=Key('id').eq(row_data["id"]))
         items = response['Items']
         if len(items) == 0:
+            row_data['created_at'] = str(datetime.now())
+            row_data['updated_at'] = str(datetime.now())
             table.put_item(Item=row_data)
         else:
-            table.update_item(
-                Key={ 'id': row_data["id"] },
-                UpdateExpression='Set abbr = :abbr, title = :title, abstract = :abstract, session_type = :session_type, speakers = :speakers, track = :track',
-                ExpressionAttributeValues={
-                    ':abbr': row_data['abbr'],
-                    ':title': row_data['title'],
-                    ':abstract': row_data['abstract'],
-                    ':session_type': row_data['session_type'],
-                    ':speakers': row_data['speakers'],
-                    ':track': row_data['track'],
-                }
-            )
+            if items[0]['abbr'] != row_data['abbr'] or items[0]['title'] != row_data['title'] or items[0]['abstract'] != row_data['abstract'] or items[0]['session_type'] != row_data['session_type'] or items[0]['speakers'] != row_data['speakers'] or items[0]['track'] != row_data['track']:
+                table.update_item(
+                    Key={ 'id': row_data["id"] },
+                    UpdateExpression='Set abbr = :abbr, title = :title, abstract = :abstract, session_type = :session_type, speakers = :speakers, track = :track, updated_at = :updated_at',
+                    ExpressionAttributeValues={
+                        ':abbr': row_data['abbr'],
+                        ':title': row_data['title'],
+                        ':abstract': row_data['abstract'],
+                        ':session_type': row_data['session_type'],
+                        ':speakers': row_data['speakers'],
+                        ':track': row_data['track'],
+                        ':updated_at': datetime.now()
+                    }
+                )
 
-    #second call
-
-    awsalb = ''
-    sessionidconnect = ''
-    for entry in page.headers['Set-Cookie'].split('; '):
-        retval.append(entry.split('=')[0])
-        if (entry.split('=')[0] == 'AWSALB'):
-            awsalb = entry.split('=')[1]
-        if (entry.split('=')[0] == 'Path' and entry.split('=')[1] == '/, SESSIONIDconnect'):
-            sessionidconnect = entry.split('=')[2]
-
-    headers["cookie"] = "DWRSESSIONID=nNNQ8ZylsbVlrLX1TUpsMzSNppm; _ga=GA1.2.603512740.1498063034; s_fid=4DF3153FC00FF9A8-00D998B2CA4F09CE; regStatus=pre-register; session-set=true; aws-priv=eyJ2IjoxLCJzdCI6MX0=; __atssc=google%3B1; s_campaign=em%7Cawsreinvent_reg_reminder%7Cem_84172%7Cevent_ev_reinvent%7Cevent%7Cglobal%7Cmult%7Cem_84172; c_m=emundefinedEmailundefined; s_cc=true; s_eVar60=em_84172; cookieAgreement=agreed; SESSIONIDportal=aaaZC9WE1bIhL_m8YXzzw; s_vn=1561578928706%26vn%3D5; s_sq=%5B%5BB%5D%5D; s_dslv=1539197425868; s_nr=1539197425870-Repeat; SESSIONIDconnect=" + sessionidconnect + "; __atuvc=0%7C37%2C0%7C38%2C0%7C39%2C0%7C40%2C34%7C41; __atuvs=5bc2a4f5bfc9bf9e000; AWSALB=" + awsalb
-
-    retval.append(headers["cookie"])
-    
-    data = {"searchType": "session", "more": "true"}
-    page = requests.post("https://www.portal.reinvent.awsevents.com/connect/processSearchFilters.do", headers=headers, data=data)
-    
-    s3 = boto3.client('s3')
-    s3.put_object(Body=page.text, Bucket='reinvent-grabs', Key='2.html')
-
-    return retval
+    return len(resultRows)
